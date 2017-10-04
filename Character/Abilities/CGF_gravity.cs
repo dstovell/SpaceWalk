d540b5d133a10abb1200b0c7a1b39f20417dfcdb
dstovell <ninja@Daniels-iMac.local>
2017-10-04 00:43:38 -0700

    Gravity kinda working now...

--- Character/Abilities/CGF_gravity.cs
+++ Character/Abilities/CGF_gravity.cs
@@ -9,6 +9,8 @@ namespace Opsive.ThirdPersonController.Abilities
 {
 	public class CGF_gravity : Ability
 	{
+		private float lerpSpeed = 10; // smoothing speed
+
 		public CGF_Character GetCharacter()
 		{
 			return m_GameObject.GetComponent<CGF_Character>();
@@ -24,13 +26,19 @@ namespace Opsive.ThirdPersonController.Abilities
 			return (this.GetCharacter() == null);
 		}
 
+		public override bool IsConcurrentAbility()
+		{
+			return true;
+		}
+
 		public override bool UpdateRotation()
 		{
 			CGF_Character character = this.GetCharacter();
 			if ((character != null) && character.IsInGravity())
 			{
 				//character.GetRotation();
-				Debug.LogError("UpdateRotation");
+				//character.transform.rotation = Quaternion.Lerp(character.transform.rotation, character.GetRotation(), lerpSpeed*Time.deltaTime);
+				character.transform.rotation = character.GetRotation();
 			}
 			return true;
 		}
--- Character/CGF_Character.cs
+++ Character/CGF_Character.cs
@@ -8,6 +8,8 @@ public class CGF_Character : MonoBehaviour
 	private Rigidbody rigid;
 	private CGF currentCGF;
 
+	public Vector3 SurfaceNormal = Vector3.up;
+
 	//Awake is called when the script instance is being loaded
 	void Awake()
 	{
@@ -23,17 +25,17 @@ public class CGF_Character : MonoBehaviour
 	//Use this for initialization
 	void Start()
 	{
-	    rigid = this.GetComponent<Rigidbody>();
+	    this.rigid = this.GetComponent<Rigidbody>();
 	}
 
-	private void CGF_OnApplyCGFEvent(CGF cgf, Rigidbody rigid, Collider coll)
+	private void CGF_OnApplyCGFEvent(CGF cgf, Rigidbody characterRB, Collider characterCollider)
 	{
-		if (this.rigid != rigid) 
+		if (this.rigid != characterRB)
 		{
 	        return;
 		}
 
-		Debug.LogError("CGF_OnApplyCGFEvent");
+		//Debug.LogError("CGF_OnApplyCGFEvent");
 
 		this.currentCGF = cgf;
 	}
@@ -45,6 +47,28 @@ public class CGF_Character : MonoBehaviour
 
 	public Quaternion GetRotation()
 	{
-		return Quaternion.identity;
+		if (!IsInGravity()) 
+		{
+			return this.transform.rotation;
+		}
+
+		Vector3 forceDir = (this.currentCGF.transform.position - this.transform.position);
+		forceDir.Normalize();
+
+		Ray ray = new Ray(this.transform.position, forceDir); // cast ray downwards
+		RaycastHit hit;
+		this.SurfaceNormal = -1 * forceDir;
+		if (Physics.Raycast(ray, out hit))
+		{ 
+			//this.SurfaceNormal = hit.normal;
+		}
+
+		// find forward direction with new surfaceNormal:
+		Vector3 targetForward = Vector3.Cross(this.transform.right, this.SurfaceNormal);
+
+		// align character to the new myNormal while keeping the forward direction:
+		Quaternion targetRot = Quaternion.LookRotation(targetForward, this.SurfaceNormal);
+
+		return targetRot;
 	}
 }
